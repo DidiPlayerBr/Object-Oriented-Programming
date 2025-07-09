d3585512b6e63c98eaa0c7d4797f223b5895ecc1
DidiPlayerBr <allanps4010@gmail.com>
2025-07-08 23:08:19 -0300

    criando a classe Triângulo

---
+++ OOPCourse/OOPCourse/Triângulo.cs
@@ -0,0 +1,11 @@
+﻿//Criando uma classe com três atributos para melhor representar um triângulo.
+
+namespace OOPCourse
+{
+    internal class Triângulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
