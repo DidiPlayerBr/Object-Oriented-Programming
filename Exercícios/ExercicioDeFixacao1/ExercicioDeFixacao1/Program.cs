9158898a1e399a815ee2919fbc7dc4b80d80795c
Allan Da Silva Gonçalves <allanps4010@gmail.com>
2025-09-01 15:47:11 -0300

    Cálculo de um Retângulo

---
+++ Exercícios/ExercicioDeFixacao1/ExercicioDeFixacao1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao1
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + ret.Perímetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercícios/ExercicioDeFixacao1/ExercicioDeFixacao1/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioDeFixacao1
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perímetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+    }
+}
