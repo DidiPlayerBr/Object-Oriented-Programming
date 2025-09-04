a62d248045663ff78d0b75034d4d149a3ffa0248
Allan Da Silva Gonçalves <allanps4010@gmail.com>
2025-09-04 10:37:55 -0300

    Exercicio de fixacao 2 e 3

---
+++ Exercícios/ExercicioDeFixacao2/ExercicioDeFixacao2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionário F = new Funcionário();
+
+            Console.Write("Nome: ");
+            F.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            F.SalarioBruto = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            F.Imposto = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + F);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            Double porcent = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + F);
+        }
+    }
+}
---
+++ Exercícios/ExercicioDeFixacao3/ExercicioDeFixacao3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno A = new Aluno();
+
+            Console.Write("Nome do alune: ");
+            A.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            A.Nota1 = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota2 = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota3 = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Nota Final = " + A.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (A.Aprovado()) {
+                Console.WriteLine("Aprovado");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " 
+                    + A.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS");
+            }
+        }
+    }
+}
